85bdad4f169383553b7ac38a22ebb954b711c3a3
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2026-08-01 18:31:11 +0600

    favicon added

--- app/(publicGroup)/layout.tsx
+++ app/(publicGroup)/layout.tsx
@@ -8,7 +8,6 @@ const PublicGroupLayout = async (
     { children }: { children: React.ReactNode }
 ) => {
     const user = await getMe();
-    console.log(user);
     return (
         <>
         <Navbar user={user} />
